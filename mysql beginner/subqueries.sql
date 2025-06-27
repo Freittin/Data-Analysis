592e58cb47a5479c2371c0f7a0778e7601b883f7
freittin <nideologies@gmail.com>
2025-06-27 19:04:12 +0300

    sql

---
+++ mysql beginner/subqueries.sql
@@ -0,0 +1,28 @@
+-- subqueries
+
+select *
+from employee_demographics
+where employee_id in
+				(select employee_id /*you cannot merge several operands using a comma*/
+					from employee_salary
+                    where dept_id = 1)
+;
+
+
+select first_name, salary,
+	(select avg(salary) 
+    from employee_salary)
+from employee_salary;
+
+select gender, avg(age), max(age), min(age), count(age)
+from employee_demographics
+group by gender;
+
+select gender, avg(`max(age)`)
+from
+		(select gender, avg(age), max(age), min(age), count(age)
+		from employee_demographics
+		group by gender) as agg_table
+group by gender
+;
+
